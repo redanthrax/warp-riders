f410d54e091bc6a0bdd78b8398006146bbcee3b2
redanthrax <redanthraxx@gmail.com>
2023-09-13 20:16:21 -0700

    sprites

--- main.go
+++ main.go
@@ -1,235 +1,129 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"os"
 
-const (
-	snakeLength = 256
-	squareSize  = 31
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-// Snake type
-type Snake struct {
+const (
+	screenWidth  = 800
+	screenHeight = 600
+	spriteSize   = 32
+)
+
+type Player struct {
 	Position rl.Vector2
-	Size     rl.Vector2
-	Speed    rl.Vector2
-	Color    rl.Color
+	Sprite   rl.Texture2D
+	FrameRec rl.Rectangle
 }
 
-// Food type
-type Food struct {
-	Position rl.Vector2
-	Size     rl.Vector2
-	Active   bool
-	Color    rl.Color
-}
-
-// Game type
 type Game struct {
-	ScreenWidth  int32
-	ScreenHeight int32
+	GameOver bool
+	Dead     bool
+	Pause    bool
 
 	FramesCounter int32
-	GameOver      bool
-	Pause         bool
+	FramesSpeed   int32
+	CurrentFrame  int32
 
-	Fruit         Food
-	Snake         []Snake
-	SnakePosition []rl.Vector2
-	AllowMove     bool
-	Offset        rl.Vector2
-	CounterTail   int
+	WindowShouldClose bool
+
+	Player Player
+}
+
+func NewGame() (g Game) {
+	g.Init()
+	return
 }
 
 func main() {
-	game := Game{}
-	game.Init()
+	game := NewGame()
+	game.GameOver = true
 
-	rl.InitWindow(game.ScreenWidth, game.ScreenHeight, "sample game: snake")
+	rl.InitWindow(screenWidth, screenHeight, "Warp Riders")
+
+	rl.InitAudioDevice()
+
+	game.Load()
 
 	rl.SetTargetFPS(60)
 
-	for !rl.WindowShouldClose() {
+	for !game.WindowShouldClose {
 		game.Update()
-
 		game.Draw()
 	}
 
+	game.Unload()
+	rl.CloseAudioDevice()
 	rl.CloseWindow()
+	os.Exit(0)
 }
 
-// Init - Initialize game
 func (g *Game) Init() {
-	g.ScreenWidth = 800
-	g.ScreenHeight = 450
+	g.Player = Player{
+		Position: rl.NewVector2(0, 0),
+		FrameRec: rl.NewRectangle(0, 0, spriteSize, spriteSize),
+	}
 
 	g.FramesCounter = 0
+	g.WindowShouldClose = false
 	g.GameOver = false
 	g.Pause = false
-
-	g.CounterTail = 1
-	g.AllowMove = false
-
-	g.Offset = rl.Vector2{}
-	g.Offset.X = float32(g.ScreenWidth % squareSize)
-	g.Offset.Y = float32(g.ScreenHeight % squareSize)
-
-	g.Snake = make([]Snake, snakeLength)
-
-	for i := 0; i < snakeLength; i++ {
-		g.Snake[i].Position = rl.NewVector2(g.Offset.X/2, g.Offset.Y/2)
-		g.Snake[i].Size = rl.NewVector2(squareSize, squareSize)
-		g.Snake[i].Speed = rl.NewVector2(squareSize, 0)
-
-		if i == 0 {
-			g.Snake[i].Color = rl.DarkBlue
-		} else {
-			g.Snake[i].Color = rl.Blue
-		}
-	}
-
-	g.SnakePosition = make([]rl.Vector2, snakeLength)
-
-	for i := 0; i < snakeLength; i++ {
-		g.SnakePosition[i] = rl.NewVector2(0.0, 0.0)
-	}
-
-	g.Fruit.Size = rl.NewVector2(squareSize, squareSize)
-	g.Fruit.Color = rl.SkyBlue
-	g.Fruit.Active = false
 }
 
-// Update - Update game
+func (g *Game) Load() {
+	g.Player.Sprite = rl.LoadTexture("assets/character/walk/walk.png")
+}
+
+func (g *Game) Unload() {
+	rl.UnloadTexture(g.Player.Sprite)
+}
+
 func (g *Game) Update() {
+	if rl.WindowShouldClose() {
+		g.WindowShouldClose = true
+	}
+
 	if !g.GameOver {
-		if rl.IsKeyPressed(rl.KeyP) {
-			g.Pause = !g.Pause
+		if rl.IsKeyPressed(rl.KeyQ) {
+			g.WindowShouldClose = true
+		}
+
+		if rl.IsKeyDown(rl.KeyLeft) {
+			g.Player.Position.X--
+		}
+
+		if rl.IsKeyDown(rl.KeyRight) {
+			g.Player.Position.X++
 		}
 
 		if !g.Pause {
-			// control
-			if rl.IsKeyPressed(rl.KeyRight) && g.Snake[0].Speed.X == 0 && g.AllowMove {
-				g.Snake[0].Speed = rl.NewVector2(squareSize, 0)
-				g.AllowMove = false
-			}
-			if rl.IsKeyPressed(rl.KeyLeft) && g.Snake[0].Speed.X == 0 && g.AllowMove {
-				g.Snake[0].Speed = rl.NewVector2(-squareSize, 0)
-				g.AllowMove = false
-			}
-			if rl.IsKeyPressed(rl.KeyUp) && g.Snake[0].Speed.Y == 0 && g.AllowMove {
-				g.Snake[0].Speed = rl.NewVector2(0, -squareSize)
-				g.AllowMove = false
-			}
-			if rl.IsKeyPressed(rl.KeyDown) && g.Snake[0].Speed.Y == 0 && g.AllowMove {
-				g.Snake[0].Speed = rl.NewVector2(0, squareSize)
-				g.AllowMove = false
-			}
-
-			// movement
-			for i := 0; i < g.CounterTail; i++ {
-				g.SnakePosition[i] = g.Snake[i].Position
-			}
-
-			if g.FramesCounter%5 == 0 {
-				for i := 0; i < g.CounterTail; i++ {
-					if i == 0 {
-						g.Snake[0].Position.X += g.Snake[0].Speed.X
-						g.Snake[0].Position.Y += g.Snake[0].Speed.Y
-						g.AllowMove = true
-					} else {
-						g.Snake[i].Position = g.SnakePosition[i-1]
-					}
-				}
-			}
-
-			// wall behaviour
-			if ((g.Snake[0].Position.X) > (float32(g.ScreenWidth) - g.Offset.X)) ||
-				((g.Snake[0].Position.Y) > (float32(g.ScreenHeight) - g.Offset.Y)) ||
-				(g.Snake[0].Position.X < 0) || (g.Snake[0].Position.Y < 0) {
-				g.GameOver = true
-			}
-
-			// collision with yourself
-			for i := 1; i < g.CounterTail; i++ {
-				if (g.Snake[0].Position.X == g.Snake[i].Position.X) && (g.Snake[0].Position.Y == g.Snake[i].Position.Y) {
-					g.GameOver = true
-				}
-			}
-
-			if !g.Fruit.Active {
-				g.Fruit.Active = true
-				g.Fruit.Position = rl.NewVector2(
-					float32(rl.GetRandomValue(0, (g.ScreenWidth/squareSize)-1)*squareSize+int32(g.Offset.X)/2),
-					float32(rl.GetRandomValue(0, (g.ScreenHeight/squareSize)-1)*squareSize+int32(g.Offset.Y)/2),
-				)
-
-				for i := 0; i < g.CounterTail; i++ {
-					for (g.Fruit.Position.X == g.Snake[i].Position.X) && (g.Fruit.Position.Y == g.Snake[i].Position.Y) {
-						g.Fruit.Position = rl.NewVector2(
-							float32(rl.GetRandomValue(0, (g.ScreenWidth/squareSize)-1)*squareSize),
-							float32(rl.GetRandomValue(0, (g.ScreenHeight/squareSize)-1)*squareSize),
-						)
-						i = 0
-					}
-				}
-			}
-
-			// collision
-			if rl.CheckCollisionRecs(
-				rl.NewRectangle(g.Snake[0].Position.X, g.Snake[0].Position.Y, g.Snake[0].Size.X, g.Snake[0].Size.Y),
-				rl.NewRectangle(g.Fruit.Position.X, g.Fruit.Position.Y, g.Fruit.Size.X, g.Fruit.Size.Y),
-			) {
-				g.Snake[g.CounterTail].Position = g.SnakePosition[g.CounterTail-1]
-				g.CounterTail += 1
-				g.Fruit.Active = false
-			}
-
-			g.FramesCounter++
+			//g.FramesCounter++
+			//if g.FramesCounter >= 8 {
+			//	g.FramesCounter = 0
+			//	g.Player.FrameRec.X = spriteSize
+			//} else {
+			//	g.Player.FrameRec.X = 0
+			//}
 		}
 	} else {
 		if rl.IsKeyPressed(rl.KeyEnter) {
 			g.Init()
-			g.GameOver = false
 		}
 	}
 }
 
-// Draw - Draw game
 func (g *Game) Draw() {
 	rl.BeginDrawing()
-
-	rl.ClearBackground(rl.RayWhite)
+	rl.ClearBackground(rl.SkyBlue)
 
 	if !g.GameOver {
-		// Draw grid lines
-		for i := int32(0); i < g.ScreenWidth/squareSize+1; i++ {
-			rl.DrawLineV(
-				rl.NewVector2(float32(squareSize*i)+g.Offset.X/2, g.Offset.Y/2),
-				rl.NewVector2(float32(squareSize*i)+g.Offset.X/2, float32(g.ScreenHeight)-g.Offset.Y/2),
-				rl.LightGray,
-			)
-		}
-
-		for i := int32(0); i < g.ScreenHeight/squareSize+1; i++ {
-			rl.DrawLineV(
-				rl.NewVector2(g.Offset.X/2, float32(squareSize*i)+g.Offset.Y/2),
-				rl.NewVector2(float32(g.ScreenWidth)-g.Offset.X/2, float32(squareSize*i)+g.Offset.Y/2),
-				rl.LightGray,
-			)
-		}
-
-		// Draw snake
-		for i := 0; i < g.CounterTail; i++ {
-			rl.DrawRectangleV(g.Snake[i].Position, g.Snake[i].Size, g.Snake[i].Color)
-		}
-
-		// Draw fruit to pick
-		rl.DrawRectangleV(g.Fruit.Position, g.Fruit.Size, g.Fruit.Color)
-
-		if g.Pause {
-			rl.DrawText("GAME PAUSED", g.ScreenWidth/2-rl.MeasureText("GAME PAUSED", 40)/2, g.ScreenHeight/2-40, 40, rl.Gray)
-		}
+		rl.DrawTextureRec(g.Player.Sprite, g.Player.FrameRec, g.Player.Position, rl.RayWhite)
+		rl.DrawText("LETS GO", 0, 0, 10, rl.Black)
 	} else {
-		rl.DrawText("PRESS [ENTER] TO PLAY AGAIN", int32(rl.GetScreenWidth())/2-rl.MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20)/2, int32(rl.GetScreenHeight())/2-50, 20, rl.Gray)
+		rl.DrawTextureRec(g.Player.Sprite, g.Player.FrameRec, g.Player.Position, rl.RayWhite)
+		rl.DrawText("Enter to start", 0, 0, 10, rl.Black)
 	}
 
 	rl.EndDrawing()
